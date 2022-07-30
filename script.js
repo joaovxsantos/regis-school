662fb50ac98b754bd39dd3932b4c93382ebc3241
João Vitor <joaovitor55670@gmail.com>
2022-07-30 15:43:52 -0300

    Final commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let submit = document.getElementById("submit")
+let input  = document.getElementById("ipt1")
+let valor = input.value
+
+
+
+
+submit.addEventListener("click", function () {
+
+    alert("Enrollment sent!")
+
+}
+)
